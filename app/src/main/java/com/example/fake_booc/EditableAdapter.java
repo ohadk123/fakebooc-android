70f7837b2126d9096bef14df87ec03a93769fbf5
A.celer <39280082+AdamCelermajer@users.noreply.github.com>
2024-03-08 02:31:32 +0200

    remove

--- app/src/main/java/com/example/fake_booc/EditableAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.example.fake_booc;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-public abstract class EditableAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<VH> {
-    abstract void editItem(int pos);
-}
